682bf784a3aadfd21e79996ea30d03f00933702c
SlkRck <slickrickistheman@hotmail.com>
2019-09-18 12:10:22 -0700

    Add files via upload

---
+++ ChangeRegistryValue.ps1
@@ -0,0 +1,4 @@
+﻿$regpath = read-host "What is the registry path (e.g. HKLM:\Software\Policies\Microsoft\) ?" #Do NOT include KEY!
+$regkey= Read-Host "What is the key you want to change?"
+$regval= read-host "What is the key value (0 or 1)?"
+Set-ItemProperty -Path $regpath -Name $regkey -Value $regval |Out-Null
